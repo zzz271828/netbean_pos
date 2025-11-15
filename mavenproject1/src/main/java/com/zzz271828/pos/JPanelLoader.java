4c7c1cdc3f172fd0e11fdb9593b74e19b5ac8c15
zzz271828 <lzha0272@student.monash.edu>
2025-11-15 16:03:58 +1100

    first commit, added basic files

---
+++ mavenproject1/src/main/java/com/zzz271828/pos/JPanelLoader.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.zzz271828.pos;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author coolsasisndu@gmail.com yt.com /techinbox
+ */
+public class JPanelLoader {
+   
+   public  void jPanelLoader(JPanel Main,JPanel setPanel){
+      Main.removeAll();
+     
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(Main);
+        Main.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(setPanel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(setPanel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE)
+        );
+        System.gc();
+    
+    }
+   
+}
---
+++ mavenproject1/src/main/java/com/zzz271828/pos/db.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.zzz271828.pos;
+
+/**
+ *
+ * @author yh-pc
+ */
+public class db {
+    public static Connection mycon() {
+        
+    }
+    
+}
