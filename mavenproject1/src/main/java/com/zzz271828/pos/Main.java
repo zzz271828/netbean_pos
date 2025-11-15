1eed15d9e0bebc01d47cb4e553c8d54035c4c047
zzz271828 <lzha0272@student.monash.edu>
2025-11-15 15:17:17 +1100

    initialize pos environment

---
+++ mavenproject1/src/main/java/com/zzz271828/pos/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.zzz271828.pos;
+
+/**
+ *
+ * @author yh-pc
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("POS System starting...(๑•̀ㅁ•́ฅ)");
+    }
+}
