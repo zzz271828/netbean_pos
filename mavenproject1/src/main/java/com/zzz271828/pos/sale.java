1ccebe212bfbd1f69f619a778e0793f103bb7ba3
zzz271828 <lzha0272@student.monash.edu>
2025-11-17 21:25:33 +1100

    feat: add due display func

--- mavenproject1/src/main/java/com/zzz271828/pos/sale.java
+++ mavenproject1/src/main/java/com/zzz271828/pos/sale.java
@@ -112,9 +112,9 @@ public class sale extends javax.swing.JPanel {
         jPanel6 = new javax.swing.JPanel();
         bill_total = new javax.swing.JTextField();
         jLabel7 = new javax.swing.JLabel();
-        jTextField8 = new javax.swing.JTextField();
+        balance = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
-        jTextField6 = new javax.swing.JTextField();
+        paid_amount = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
         jButton4 = new javax.swing.JButton();
 
@@ -350,11 +350,11 @@ public class sale extends javax.swing.JPanel {
         jLabel7.setFont(new java.awt.Font("Helvetica Neue", 1, 14)); // NOI18N
         jLabel7.setText("Total Amount : ");
 
-        jTextField8.setFont(new java.awt.Font("Helvetica Neue", 1, 14)); // NOI18N
-        jTextField8.setText("00");
-        jTextField8.addActionListener(new java.awt.event.ActionListener() {
+        balance.setFont(new java.awt.Font("Helvetica Neue", 1, 14)); // NOI18N
+        balance.setText("00");
+        balance.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField8ActionPerformed(evt);
+                balanceActionPerformed(evt);
             }
         });
 
@@ -371,7 +371,7 @@ public class sale extends javax.swing.JPanel {
                     .addGroup(jPanel6Layout.createSequentialGroup()
                         .addComponent(jLabel8)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField8, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(balance, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel6Layout.createSequentialGroup()
                         .addComponent(jLabel7)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -388,15 +388,20 @@ public class sale extends javax.swing.JPanel {
                 .addGap(34, 34, 34)
                 .addGroup(jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel8)
-                    .addComponent(jTextField8, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(balance, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(14, 14, 14))
         );
 
-        jTextField6.setFont(new java.awt.Font("Helvetica Neue", 1, 14)); // NOI18N
-        jTextField6.setText("00");
-        jTextField6.addActionListener(new java.awt.event.ActionListener() {
+        paid_amount.setFont(new java.awt.Font("Helvetica Neue", 1, 14)); // NOI18N
+        paid_amount.setText("00");
+        paid_amount.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField6ActionPerformed(evt);
+                paid_amountActionPerformed(evt);
+            }
+        });
+        paid_amount.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                paid_amountKeyReleased(evt);
             }
         });
 
@@ -411,7 +416,7 @@ public class sale extends javax.swing.JPanel {
                 .addGap(22, 22, 22)
                 .addComponent(jLabel6)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(paid_amount, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 437, Short.MAX_VALUE)
                 .addComponent(jPanel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(27, 27, 27))
@@ -424,7 +429,7 @@ public class sale extends javax.swing.JPanel {
                         .addGap(29, 29, 29)
                         .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel6)
-                            .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(paid_amount, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(jPanel3Layout.createSequentialGroup()
                         .addGap(20, 20, 20)
                         .addComponent(jPanel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -486,17 +491,17 @@ public class sale extends javax.swing.JPanel {
         // TODO add your handling code here:
     }//GEN-LAST:event_qtyActionPerformed
 
-    private void jTextField6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField6ActionPerformed
+    private void paid_amountActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_paid_amountActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField6ActionPerformed
+    }//GEN-LAST:event_paid_amountActionPerformed
 
     private void bill_totalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bill_totalActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_bill_totalActionPerformed
 
-    private void jTextField8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField8ActionPerformed
+    private void balanceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_balanceActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField8ActionPerformed
+    }//GEN-LAST:event_balanceActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // add to cart operation details
@@ -589,8 +594,21 @@ public class sale extends javax.swing.JPanel {
         prod_tot_cal();
     }//GEN-LAST:event_qtyKeyReleased
 
+    private void paid_amountKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_paid_amountKeyReleased
+        // when typed in the paid amount
+        Double paid = Double.valueOf(paid_amount.getText().toString());
+        Double tot = Double.valueOf(bill_total.getText().toString());
+        
+        Double balance_due;
+        balance_due = tot - paid;
+        
+        balance.setText(String.valueOf(balance_due));
+        
+    }//GEN-LAST:event_paid_amountKeyReleased
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField balance;
     private javax.swing.JLabel bar_code;
     private javax.swing.JTextField bill_total;
     private javax.swing.JComboBox<String> com_cus;
@@ -617,8 +635,7 @@ public class sale extends javax.swing.JPanel {
     private javax.swing.JPanel jPanel5;
     private javax.swing.JPanel jPanel6;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField6;
-    private javax.swing.JTextField jTextField8;
+    private javax.swing.JTextField paid_amount;
     private javax.swing.JTextField qty;
     private javax.swing.JTable sale_prod_tb;
     private javax.swing.JLabel total_price;
